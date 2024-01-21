0a41d1b9384c1987919cd6e5bd3a702c20ab9768
Omar Mohsin <Omarabdessalam4748@gmail.com>
2024-01-21 09:44:29 +0300

    adding some improvment

--- components/Home/WorkspaceSection.tsx
+++ components/Home/WorkspaceSection.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import SettingsIcon from "@mui/icons-material/Settings";
+import { useRouter } from "next/navigation";
 function WorkspaceSection() {
+  const router = useRouter();
+
+  const moveToWorkspaceSetting = (id: number) => {
+    router.push(`/workspace-settings/${id}`);
+  };
   return (
     <div className="w-full md:w-1/2 mt-4 md:mt-0">
       <h1 className="text-2xl md:text-3xl mb-4">Other Workspaces</h1>
       <div className="mb-4 flex flex-col md:flex-row justify-between items-center">
+        {/* it will be  a map here to display all workspaces that we have */}
         <div>
           <h2 className="text-lg font-bold">Workspace 2</h2>
         </div>
@@ -18,7 +25,10 @@ function WorkspaceSection() {
         {/* icons */}
         <div className="flex items-center gap-2">
           <NotificationsIcon className="cursor-pointer" />
-          <SettingsIcon className="cursor-pointer" />
+          <SettingsIcon
+            onClick={() => moveToWorkspaceSetting(1)}
+            className="cursor-pointer"
+          />
           <DeleteIcon className="cursor-pointer" />
         </div>
         <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md">
